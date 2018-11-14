5a566a635605084b1ca79e7af0bf1f5f4bb2f3d2
leostarzz <neytron888@gmail.com>
2018-11-14 13:25:24 +0300

    Update lecture4/index.js

--- lecture4/index.js
+++ lecture4/index.js
@@ -1,236 +1,113 @@
-document.addEventListener("DOMContentLoaded", function () {
+var calculator = (function () {
+	document.addEventListener("DOMContentLoaded", function () {
+		var result = '';
+		var finalResult = 0;
+		var area = document.querySelector(".result");
+		var buttonEqual = document.querySelector(".button_equal");
+		var buttonAC = document.querySelector(".button_ac");
+		var history = document.querySelector(".history");
+		var buttonClear = document.querySelector("button");
+		var buttons = document.querySelector(".buttons");
 
-	var result = '';
-	var finalResult = 0;
-	var area = document.querySelector(".result");
-	var button1 = document.querySelector(".button1");
-	var button2 = document.querySelector(".button2");
-	var button3 = document.querySelector(".button3");
-	var button4 = document.querySelector(".button4");
-	var button5 = document.querySelector(".button5");
-	var button6 = document.querySelector(".button6");
-	var button7 = document.querySelector(".button7");
-	var button8 = document.querySelector(".button8");
-	var button9 = document.querySelector(".button9");
-	var button0 = document.querySelector(".button0");
-	var buttonMinus = document.querySelector(".button_minus");
-	var buttonPlus = document.querySelector(".button_plus");
-	var buttonDivide = document.querySelector(".button_divide");
-	var buttonMultiply = document.querySelector(".button_multiply");
-	var buttonEqual = document.querySelector(".button_equal");
-	var buttonAC = document.querySelector(".button_ac");
-	var history = document.querySelector(".history");
-	var buttonLeftbracket = document.querySelector(".button_leftbracket");
-	var buttonRightbracket = document.querySelector(".button_rightbracket");
-	var buttonPoint = document.querySelector(".button_point");
-	var buttonClear = document.querySelector("button");
-
-	var data = JSON.parse(localStorage.getItem('items')) || [];
-	console.log(data);
-	if (data !== null) {
-		data.forEach(item => {
-			var div = document.createElement("div");
-			div.innerHTML += item;
-			history.appendChild(div);
-		});
-	}
-
-	window.addEventListener('keydown', function (event) {
-		switch (event.keyCode) {
-			case 97:
-				result = result + '1';
-				area.innerHTML = result;
-				break;
-			case 98:
-				result = result + '2';
-				area.innerHTML = result;
-				break;
-			case 99:
-				result = result + '3';
-				area.innerHTML = result;
-				break;
-			case 100:
-				result = result + '4';
-				area.innerHTML = result;
-				break;
-			case 101:
-				result = result + '5';
-				area.innerHTML = result;
-				break;
-			case 102:
-				result = result + '6';
-				area.innerHTML = result;
-				break;
-			case 103:
-				result = result + '7';
-				area.innerHTML = result;
-				break;
-			case 104:
-				result = result + '8';
-				area.innerHTML = result;
-				break;
-			case 105:
-				result = result + '9';
-				area.innerHTML = result;
-				break;
-			case 96:
-				result = result + '0';
-				area.innerHTML = result;
-				break;
-			case 107:
-				result = result + '+';
-				area.innerHTML = result;
-				break;
-			case 109:
-				result = result + '-';
-				area.innerHTML = result;
-				break;
-			case 106:
-				result = result + '*';
-				area.innerHTML = result;
-				break;
-			case 111:
-				result = result + '/';
-				area.innerHTML = result;
-				break;
-			case 110:
-				result = result + '.';
-				area.innerHTML = result;
-				break;
-			case 13:
-				try {
-					finalResult = eval(result);
-				} catch (err) {
-					alert("Введите правильное выражение!");
-				}
-				area.innerHTML = finalResult;
+		var data = JSON.parse(localStorage.getItem('items')) || [];
+		if (data !== null) {
+			data.forEach(item => {
 				var div = document.createElement("div");
-				div.innerHTML += result + ' = ' + finalResult;
-				data.push(result + ' = ' + finalResult);
+				div.innerHTML += item;
 				history.appendChild(div);
-				localStorage.setItem('items', JSON.stringify(data));
-				result = '';
-				finalResult = '';
-				break;
+			});
 		}
 
-	}, true);
+		window.addEventListener('keydown', function (event) {
+			switch (event.keyCode) {
+				case 96: case 97: case 98: case 99: case 100: case 101: case 102: case 103: case 104: case 105:
+					result = result + (event.keyCode - 96).toString();
+					area.innerHTML = result;
+					break;
+				case 107:
+					result = result + '+';
+					area.innerHTML = result;
+					break;
+				case 109:
+					result = result + '-';
+					area.innerHTML = result;
+					break;
+				case 106:
+					result = result + '*';
+					area.innerHTML = result;
+					break;
+				case 111:
+					result = result + '/';
+					area.innerHTML = result;
+					break;
+				case 110:
+					result = result + '.';
+					area.innerHTML = result;
+					break;
+				case 13:
+					try {
+						finalResult = eval(result);
+					} catch (err) {
+						alert("Введите правильное выражение!");
+					}
+					area.innerHTML = finalResult;
+					var div = document.createElement("div");
+					div.innerHTML += result + ' = ' + finalResult;
+					data.push(result + ' = ' + finalResult);
+					history.appendChild(div);
+					localStorage.setItem('items', JSON.stringify(data));
+					result = '';
+					finalResult = '';
+					break;
+			}
 
-	button1.addEventListener('click', function () {
-		result = result + '1';
-		area.innerHTML = result;
+		}, true);
+
+		buttons.addEventListener('click', function () {
+			if (event.target.innerHTML !== 'AC' && event.target.innerHTML !== '=') {
+				if (event.target.innerHTML === '×') {
+					result = result + '*';
+					area.innerHTML = result;
+				} else if (event.target.innerHTML === '÷') {
+					result = result + '/';
+					area.innerHTML = result;
+				} else {
+					result = result + event.target.innerHTML;
+					area.innerHTML = result;
+				}
+			}
+		});
+
+		buttonEqual.addEventListener('click', function () {
+			try {
+				finalResult = eval(result);
+			} catch (err) {
+				alert("Введите правильное выражение!");
+			}
+			area.innerHTML = finalResult;
+			var div = document.createElement("div");
+			div.innerHTML += result + ' = ' + finalResult;
+			data.push(result + ' = ' + finalResult);
+			history.appendChild(div);
+			localStorage.setItem('items', JSON.stringify(data));
+			result = '';
+			finalResult = '';
+		});
+
+		buttonAC.addEventListener('click', function () {
+			finalResult = '';
+			result = '';
+			area.innerHTML = '';
+		});
+
+		buttonClear.addEventListener('click', function () {
+			localStorage.clear();
+			while (history.firstChild) {
+				history.removeChild(history.firstChild);
+			}
+			history.innerHTML = 'History';
+			data = [];
+
+		});
 	});
-
-	button2.addEventListener('click', function () {
-		result = result + '2';
-		area.innerHTML = result;
-	});
-
-	button3.addEventListener('click', function () {
-		result = result + '3';
-		area.innerHTML = result;
-	});
-
-	button4.addEventListener('click', function () {
-		result = result + '4';
-		area.innerHTML = result;
-	});
-
-	button5.addEventListener('click', function () {
-		result = result + '5';
-		area.innerHTML = result;
-	});
-
-	button6.addEventListener('click', function () {
-		result = result + '6';
-		area.innerHTML = result;
-	});
-
-	button7.addEventListener('click', function () {
-		result = result + '7';
-		area.innerHTML = result;
-	});
-
-	button8.addEventListener('click', function () {
-		result = result + '8';
-		area.innerHTML = result;
-	});
-
-	button9.addEventListener('click', function () {
-		result = result + '9';
-		area.innerHTML = result;
-	});
-
-	button0.addEventListener('click', function () {
-		result = result + '0';
-		area.innerHTML = result;
-	});
-
-	buttonMinus.addEventListener('click', function () {
-		result = result + '-';
-		area.innerHTML = result;
-	});
-
-	buttonPlus.addEventListener('click', function () {
-		result = result + '+';
-		area.innerHTML = result;
-	});
-
-	buttonMultiply.addEventListener('click', function () {
-		result = result + '*';
-		area.innerHTML = result;
-	});
-
-	buttonDivide.addEventListener('click', function () {
-		result = result + '/';
-		area.innerHTML = result;
-	});
-
-	buttonEqual.addEventListener('click', function () {
-		try {
-			finalResult = eval(result);
-		} catch (err) {
-			alert("Введите правильное выражение!");
-		}
-		area.innerHTML = finalResult;
-		var div = document.createElement("div");
-		div.innerHTML += result + ' = ' + finalResult;
-		data.push(result + ' = ' + finalResult);
-		history.appendChild(div);
-		localStorage.setItem('items', JSON.stringify(data));
-		result = '';
-		finalResult = '';
-	});
-
-	buttonAC.addEventListener('click', function () {
-		finalResult = '';
-		result = '';
-		area.innerHTML = '';
-	});
-
-	buttonLeftbracket.addEventListener('click', function () {
-		result = result + '(';
-		area.innerHTML = result;
-	});
-
-	buttonRightbracket.addEventListener('click', function () {
-		result = result + ')';
-		area.innerHTML = result;
-	});
-
-	buttonPoint.addEventListener('click', function () {
-		result = result + '.';
-		area.innerHTML = result;
-	});
-
-	buttonClear.addEventListener('click', function () {
-		localStorage.clear();
-		while (history.firstChild) {
-			history.removeChild(history.firstChild);
-		}
-		history.innerHTML = 'History';
-		data = [];
-
-	});
-
-});
+})();
