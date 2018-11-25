2f9fb3ef6484cbc3c8f0e780e415f7222b4298cf
leostarzz <neytron888@gmail.com>
2018-11-25 23:15:05 +0300

    Update utils.js

--- lecture6/utils.js
+++ lecture6/utils.js
@@ -1,7 +1,41 @@
 var utils = (function () {
-  return {
-		getRandomInRange: function(min, max) {
+	return {
+		getRandomInRange(min, max) {
 			return Math.floor(Math.random() * (max - min + 1)) + min;
+		},
+
+		isProjectExists(projectName, projects, freeProjects) {
+			for (var i = 0; i < projects.length; i++) {
+				if (projectName === projects[i].name) {
+					return true;
+				}
+			}
+
+			for (var j = 0; j < freeProjects.length; j++) {
+				if (projectName === freeProjects[j].name) {
+					return true;
+				}
+			}
+
+			return false;
+		},
+
+		isManagerExists(managerName, managerSurname, managers) {
+			for (var i = 0; i < managers.length; i++) {
+				if (managerName === managers[i].name && managerSurname === managers[i].surname) {
+					return true;
+				}
+			}
+			return false;
+		},
+
+		isDeveloperExists(developerName, developerSurname, developers) {
+			for (var i = 0; i < developers.length; i++) {
+				if (developerName === developers[i].name && developerSurname === developers[i].surname) {
+					return true;
+				}
+			}
+			return false;
 		}
 	}
 })();
