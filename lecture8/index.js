c6b67fdb4db93a18ac5057b6ead6d4c134249c4b
leostarzz <neytron888@gmail.com>
2018-11-28 14:18:35 +0300

    first task completed

--- lecture8/index.js
+++ lecture8/index.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const stdin = process.openStdin();
-var stream = fs.createWriteStream('./tmp.txt');
+const stream = fs.createWriteStream('./tmp.txt');
 
 stdin.on('data', (data) => {
-	if (data.toString().trim() === 'exit') {
-		fs.unlink('./tmp.txt');
-		process.exit();
-	} else {
-		stream.write(data.toString().trim() + '\n');
-	}
+  if (data.toString().trim() === 'exit') {
+    fs.unlink('./tmp.txt');
+    process.exit();
+  } else {
+    stream.write(data.toString().trim() + '\n');
+  }
 });
 
 
