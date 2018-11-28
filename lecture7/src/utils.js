2dfcdce735dbd15a5eadbe1d55937fbbd6c9e2e3
leostarzz <neytron888@gmail.com>
2018-11-28 14:52:55 +0300

    added array.find usage

--- lecture7/src/utils.js
+++ lecture7/src/utils.js
@@ -1,39 +1,17 @@
 export function getRandomInRange(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 export function isProjectExists(projectName, projects, freeProjects) {
-	for (var i = 0; i < projects.length; i++) {
-		if (projectName === projects[i].name) {
-			return true;
-		}
-	}
-
-	for (var j = 0; j < freeProjects.length; j++) {
-		if (projectName === freeProjects[j].name) {
-			return true;
-		}
-	}
-
-	return false;
+  return projects.find((item) => item.name === projectName) || freeProjects.find((item) => item.name === projectName);
 }
 
 export function isManagerExists(managerName, managerSurname, managers) {
-	for (var i = 0; i < managers.length; i++) {
-		if (managerName === managers[i].name && managerSurname === managers[i].surname) {
-			return true;
-		}
-	}
-	return false;
+  return managers.find((item) => item.name === managerName && item.surname === managerSurname);
 }
 
 export function isDeveloperExists(developerName, developerSurname, developers) {
-	for (var i = 0; i < developers.length; i++) {
-		if (developerName === developers[i].name && developerSurname === developers[i].surname) {
-			return true;
-		}
-	}
-	return false;
+  return developers.find((item) => item.name === developerName && item.surname === developerSurname);
 }
 
 
